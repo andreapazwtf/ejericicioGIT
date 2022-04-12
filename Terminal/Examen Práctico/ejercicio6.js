3c6c66c9688957c256c2cbc8638a16b83f44b789
andreav <andrea.pazvarela@gmail.com>
2022-04-12 18:22:32 -0400

    arreglado

--- Terminal/Examen Práctico/ejercicio6.js
+++ Terminal/Examen Práctico/ejercicio6.js
@@ -1,6 +1,6 @@
 let indiceDePunto = cadena.indexOf('.');
 if(indiceDePunto < 0)
-  return cadena.toUpperCase();
+  returncadena.toUpperCase();
   let inicioHastaPunto = cadena.substring(0, indiceDePunto); 
   let restoDespuesDePunto = cadena.substring(indiceDePunto, cadena.length());
   let inicioHastaPuntoConMayus = inicioHastaPunto.toUpperCase();
